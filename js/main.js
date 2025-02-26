76dd17955ae96876edeb967f98ec2cde1191ce7c
youseif-elshreif <105569390+youseif-elshreif@users.noreply.github.com>
2025-02-26 20:13:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -121,7 +121,7 @@ function cahngePic() {
             while (test==picNum) {
                 picNum = Math.floor(Math.random() * 6)+1;
             }
-            pic.style.backgroundImage= `url("/images/landing${picNum}.jpg")`;
+            pic.style.backgroundImage= `url("https://youseif-elshreif.github.io/New_version_T1hCJ/images/landing${picNum}.jpg")`;
             test=picNum;
         }, 2000);
     }
